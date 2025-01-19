73e08b6f0334b86397849f4426741dd360ca5e42
andrea <andypedini02@gmail.com>
2025-01-19 01:45:10 +0100

    .

--- src/db/createJson.js
+++ src/db/createJson.js
@@ -2,29 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const parse = require("csv-parse").parse;
 
-let earthquakes = [];
-
-function createJson() {
-
-    fs.createReadStream('earthquakes_test.csv')
-    .pipe(parse({ delimiter: ",", from_line: 2 }))
-    .on("data", function (row) {
-        earthquakes.push(row);
-    })
-    .on("end", function () {
-        earthquakes.reverse();
-        console.log("Parsing effettuato con successo");
-        fs.writeFileSync("earthquakes.json", JSON.stringify(earthquakes));
-        console.log("File .json creato con successo!");
-    })
-    .on("error", function (error) {
-        console.log("Errore: "+error);
-    });
-}
-
 function createJsonWithKeys() {
     // Leggi il file CSV
-    const csvFilePath = path.join(__dirname, 'earthquakes_test.csv');
+    const csvFilePath = path.join(__dirname, 'earthquakes.csv');
     const csvContent = fs.readFileSync(csvFilePath, 'utf-8');
 
     // Splitta il contenuto del file in righe
