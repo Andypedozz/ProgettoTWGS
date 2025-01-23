0e9f19b72f0db8f37edc1e3aa7fdb638f2be26b6
andrea <andypedini02@gmail.com>
2025-01-23 13:20:24 +0100

    .

--- src/db/createJson.js
+++ src/db/createJson.js
@@ -2,6 +2,45 @@ const fs = require("fs");
 const path = require("path");
 const parse = require("csv-parse").parse;
 
+function createJson() {
+    // Leggi il file CSV
+    const csvFilePath = path.join(__dirname, 'earthquakes.csv');
+    const csvContent = fs.readFileSync(csvFilePath, 'utf-8');
+
+    // Splitta il contenuto del file in righe
+    const lines = csvContent.split('\r');
+    
+    // Ottieni i nomi delle colonne dalla prima riga
+    const headers = lines[0].split(',');
+    // lines.reverse();
+
+    // Crea un array per contenere i dati JSON
+    const jsonData = [];
+
+    // Itera sulle righe successive (dalla seconda riga in poi)
+    for (let i = 1; i < lines.length; i++) {
+        const line = lines[i].trim();
+        if (line) {
+            const values = line.split(',');
+            const jsonEntry = {};
+
+            // Popola il dizionario per ogni riga
+            headers.forEach((header, index) => {
+                jsonEntry[header] = values[index];
+            });
+
+            // Aggiungi l'oggetto JSON all'array
+            jsonData.push(jsonEntry);
+        }
+    }
+
+    // Converti l'array in una stringa JSON
+    const jsonResult = JSON.stringify(jsonData, null, 4);
+
+    // Salva il risultato in un file JSON (opzionale)
+    fs.writeFileSync('earthquakes.json', jsonResult);
+}
+
 function createJsonReplaceIds() {
     // Leggi il file CSV
     const csvFilePath = path.join(__dirname, 'earthquakes.csv');
@@ -46,4 +85,4 @@ function createJsonReplaceIds() {
     fs.writeFileSync('earthquakes.json', jsonResult);
 }
 
-createJsonReplaceIds();
+createJson();
